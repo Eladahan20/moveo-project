a4a434de334bd684ee5ff3565a38b8c16e367099
Elad Dahan <eladdahan20@gmail.com>
2023-01-27 18:03:21 +0200

    index set up

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 const express = require('express');
+const request = require('request');
+const path = require('path');
+
 const port = process.env.PORT || 8080;
 const app = express();
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, '/index.html'));
+  });
 
 
 app.listen(port, () => {
