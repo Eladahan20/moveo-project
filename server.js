108bdf3adcf60e4437800e240c4d404abf0cbab6
Elad Dahan <eladdahan20@gmail.com>
2023-01-28 14:54:17 +0200

    fixing sessions

--- server.js
+++ server.js
@@ -1,54 +1,57 @@
-const express = require('express');
-const request = require('request');
-const path = require('path');
+const express = require("express");
+const request = require("request");
+const path = require("path");
 const app = express();
+
+// load DB
 const codeblocks = require("./data/codeblocks.js");
 
-const server = require('http').createServer(app);
-const WebSocket = require('ws');
+//Set up Websocket
+const server = require("http").createServer(app);
+const WebSocket = require("ws");
+const wss = new WebSocket.Server({ server: server });
 
-
-const wss = new WebSocket.Server({ server:server });
-
-wss.on('connection', function connection(ws) {
-    console.log('Connection made');
-    // console.log(wss.clients.size)
-    if (wss.clients.size === 1) {
-        console.log('Mentor Logged In')
-        ws.send('Mentor');
-    } else {
-        console.log('Student Logged In')
-        ws.on('message', function incoming(message) {
-            wss.clients.forEach(function each(client) {
-              if (client.readyState === WebSocket.OPEN) {
-                client.send(message.toString());
-              }
-            });
-        });
-    }
-
+//Websocket Events Handler
+wss.on("connection", function connection(ws) {
+  console.log("Connection made");
+  //Client check (Mentor/Student), and implement states
+  if (wss.clients.size === 1) {
+    console.log("Mentor Logged In");
+    ws.send("Mentor");
+  } else {
+    console.log("Student Logged In");
+    ws.on("message", function incoming(message) {
+      wss.clients.forEach(function each(client) {
+        if (client.readyState === WebSocket.OPEN) {
+          client.send(message.toString());
+        }
+      });
+    });
+  }
+  //Connection Ends
+  ws.on("close", (code, reason) => {
+    console.log(wss.clients.size);
+    console.log("client disconnected from server");
   });
-
-const port = process.env.PORT || 8080;
-app.set('view engine', 'ejs');
-app.use(express.static('views'));
-
-
-app.get('/', function(req, res) {
-    res.render('index', {codeblocks: codeblocks});
-  });
-
-  app.get("/code/:codeblockId", (req, res) => {
-    const codeblockId = req.params.codeblockId;
-    const codeblock = codeblocks.find(c => c.id === parseInt(codeblockId));
-    res.render("code", { codeblock });
 });
 
-  app.get('/code1', function(req, res) {
-    res.sendFile(path.join(__dirname, 'Public/code1.html'));
-  });
+//App sets and routes
+app.set("view engine", "ejs");
+app.use(express.static("views"));
+
+app.get("/", function (req, res) {
+  res.render("index", { codeblocks: codeblocks });
+});
+
+app.get("/code/:codeblockId", (req, res) => {
+  const codeblockId = req.params.codeblockId;
+  const codeblock = codeblocks.find((c) => c.id === parseInt(codeblockId));
+  res.render("code", { codeblock });
+});
+
+
+const port = process.env.PORT || 8080;
 
 server.listen(port, () => {
-    console.log('Server started');
-  });
-  
+  console.log("Server started");
+});
--- views/js/Websocket.js
+++ views/js/Websocket.js
@@ -1,10 +1,16 @@
-    // Create WebSocket connection.
-    // const socket = new WebSocket('ws://localhost:8080');
+// Create WebSocket connection.
+
+const socket = new WebSocket('wss://moveo-p.herokuapp.com/code');
+// const socket = new WebSocket("ws://localhost:8080");
 
 // Connection opened
-socket.addEventListener('open', function (event) {
-console.log('Connected to WS Server')
+socket.addEventListener("open", function (event) {
+  console.log("Connected to WS Server");
 });
+
+socket.onclose = (event) => {
+  console.log(`Connection closed: ${event.code} ${event.reason}`);
+};
 
 // Listen for messages
 // socket.addEventListener('message', function (event) {
@@ -12,25 +18,27 @@ console.log('Connected to WS Server')
 // });
 
 const sendMessage = () => {
-socket.terminate();
-}
-var editableCode = document.getElementById("editableCode");
-var role = document.getElementById("role");
-editableCode.addEventListener("input", function() {
-// Send the updated code to the server via the WebSocket connection
-socket.send(this.innerHTML);
-});
-// Listen for messages from the server
-socket.onmessage = function(event) {
-// Update the editable <code> element with the latest code from the server
-if (event.data.toString() == "Mentor") {
-editableCode.setAttribute("contenteditable",false);
-role.innerHTML = "Mentor";
-} else {
-editableCode.innerHTML = event.data.toString();
-}
+  console.log(socket);
+  socket.close();
+  console.log(socket);
 };
 
-window.addEventListener("beforeunload", function(event) {
-    websocket.close(1000, "Client requested closure");
-  });
+
+var editableCode = document.getElementById("editableCode");
+var role = document.getElementById("role");
+
+editableCode.addEventListener("input", function () {
+  // Send the updated code to the server via the WebSocket connection
+  socket.send(this.innerHTML);
+});
+
+// Listen for messages from the server
+socket.onmessage = function (event) {
+  // Update the editable <code> element with the latest code from the server
+  if (event.data.toString() == "Mentor") {
+    editableCode.setAttribute("contenteditable", false);
+    role.innerHTML = "Mentor";
+  } else {
+    editableCode.innerHTML = event.data.toString();
+  }
+};
