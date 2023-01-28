21b481d5d9dbc53c759ed447431350746df07c7e
Elad Dahan <eladdahan20@gmail.com>
2023-01-28 18:27:09 +0200

    change to textconent

--- views/js/Websocket.js
+++ views/js/Websocket.js
@@ -1,7 +1,7 @@
 // Create WebSocket connection.
 
-const socket = new WebSocket('wss://moveo-p.herokuapp.com/code');
-// const socket = new WebSocket("ws://localhost:8080");
+// const socket = new WebSocket('wss://moveo-p.herokuapp.com/code');
+const socket = new WebSocket("ws://localhost:8080");
 
 // Connection opened
 socket.addEventListener("open", function (event) {
@@ -32,17 +32,17 @@ editableCode.addEventListener("input", function () {
         document.getElementById("smiley").style.display = "block";
     }
   // Send the updated code to the server via the WebSocket connection
-    socket.send(this.innerHTML);
+    socket.send(this.textContent);
 });
 
 // Listen for messages from the server
 socket.onmessage = function (event) {
   // Update the editable <code> element with the latest code from the server
   if (event.data.toString() == "Mentor") {
-    editableCode.setAttribute("contenteditable", false);
+    // editableCode.setAttribute("contenteditable", false);
     role.innerHTML = "Mentor";
   } else {
     role.innerHTML = "Student";
-    editableCode.innerHTML = event.data.toString();
+    editableCode.textContent = event.data.toString();
   }
 };
