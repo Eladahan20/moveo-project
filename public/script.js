1aef1adcedc4ae171bdcb47b9b84138426538bc8
Elad Dahan <eladdahan20@gmail.com>
2023-01-29 18:46:42 +0200

    smiley fix

--- public/script.js
+++ public/script.js
@@ -1,7 +1,7 @@
 // Create WebSocket connection.
 
-const socket = new WebSocket('wss://moveo-p.herokuapp.com/code');
-// const socket = new WebSocket("ws://localhost:8080");
+// const socket = new WebSocket('wss://moveo-p.herokuapp.com/code');
+const socket = new WebSocket("ws://localhost:8080");
 
 // Connection opened
 socket.addEventListener("open", function (event) {
@@ -27,20 +27,23 @@ var editableCode = document.getElementById("editableCode");
 var role = document.getElementById("role");
 
 editableCode.addEventListener("input", function () {
-    var solution = document.getElementById('codeSolution').innerHTML;
-    if (this.innerHTML === solution) {
+    var solution = document.getElementById('codeSolution').textContent;
+    // console.log(this.innerHTML);
+    // console.log(solution);
+    if (this.textContent == solution) {
+      console.log('true');
         document.getElementById("smiley").style.display = "block";
     }
   // Send the updated code to the server via the WebSocket connection
-    socket.send(this.innerHTML);
+    socket.send(this.textContent);
 });
 
 // Listen for messages from the server
 socket.onmessage = function (event) {
   // Update the editable <code> element with the latest code from the server
   if (event.data.toString() == "Mentor") {
-    editableCode.setAttribute("contenteditable", false);
+    // editableCode.setAttribute("contenteditable", false);
   } else {
-    editableCode.innerHTML = event.data.toString();
+    editableCode.textContent = event.data.toString();
   }
 };
